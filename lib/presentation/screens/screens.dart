6c7f4842d9e628baccd9564329ca8c30f07dd463
donotr <tr1900@ues.edu.sv>
2023-10-03 12:43:06 -0600

    fix: Se agregaron los widgets faltantes y los assets images

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,5 @@
 export 'package:widget_formato/presentation/screens/animated/animated_screen.dart';
+export 'package:widget_formato/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widget_formato/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widget_formato/presentation/screens/cards/cards_screen.dart';
 export 'package:widget_formato/presentation/screens/home/home_screen.dart';
